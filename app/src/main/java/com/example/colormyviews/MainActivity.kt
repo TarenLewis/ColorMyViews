e6f347c1f32eb086b8c1179c1826e153f8b2428e
taren <taren.lewis@gmail.com>
2020-05-19 17:24:10 -0500

    First commit ColorMyViews, gitignore added

---
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.colormyviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+//bias: position of element along horizontal or vertical axis
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
